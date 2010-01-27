388f0a0e936d94f3cfa5f253ab516731ae1b5c74
Paul Swartz <paulswartz@gmail.com>
2010-01-27 12:42:05 -0500

    Bug #12782: Categories Link Should be Disabled for those with Javascript

--- static/4/js/submit_video.js
+++ static/4/js/submit_video.js
@@ -9,6 +9,7 @@ function setup_submit_callbacks(wrap, result) {
 }
 
 $(document).ready(function() {
+    $('#nav li.categories').css('cursor', 'default').click(function() {return false;});
     $("a[rel]").overlay({
         expose: '#499ad9',
         effect: 'apple',
