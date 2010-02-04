34300f417861bfbe46c93e7c2a5f6ce859432c61
Paul Swartz <paulswartz@gmail.com>
2010-02-04 14:14:29 -0500

    Bug #12833: reCAPTCHA breaks comment popup

--- static/4/js/comment.js
+++ static/4/js/comment.js
@@ -1,14 +1,33 @@
-$(document).ready(function() {
-    $(".comment_form form").ajaxForm({success: function (response) {
-        response = $(response);
-        if (response.children('#next').length) {
-            location.href = response.children('#next').attr('href');
-        } else {
-            $("#overlay").find('.contentWrap').html(response);
-            wrap = $("#overlay").overlay({api: true});
-            wrap.getContent().find('form:eq(0)').ajaxForm(
-                function(result) {setup_submit_callbacks(wrap, result);});
-            wrap.load();
+function comment_load(response) {
+    response = $(response);
+    if (response.children('#next').length) {
+        location.href = response.children('#next').attr('href');
+    } else {
+        response.find('script').remove();
+        $("#overlay").find('.contentWrap').html(response);
+        if (typeof Recaptcha != 'undefined') {
+            recaptcha_url = $('script[src^=http://api.recaptcha.net/challenge]').attr('src');
+            recaptcha_key = /k=(\w*)/.exec(recaptcha_url)[1];
+            recaptcha_wrapper = document.createElement('div');
+            Recaptcha.create(recaptcha_key, recaptcha_wrapper, RecaptchaOptions);
+            $("#overlay input[name=recaptcha_ip_field]").after(recaptcha_wrapper)
         }
-    }});
+        wrap = $("#overlay").overlay({api: true,
+                                         onClose: comment_close});
+        wrap.getContent().find('form:eq(0)').ajaxForm(comment_load);
+        wrap.load();
+    }
+}
+function comment_close() {
+    $("#overlay .contentWrap").empty();
+    if (typeof Recaptcha != 'undefined') {
+        recaptcha_url = $('script[src^=http://api.recaptcha.net/challenge]').attr('src');
+        recaptcha_key = /k=(\w*)/.exec(recaptcha_url)[1];
+        recaptcha_wrapper = document.createElement('div');
+        Recaptcha.create(recaptcha_key, recaptcha_wrapper, RecaptchaOptions);
+        $("input[name=recaptcha_ip_field]").after(recaptcha_wrapper)
+    }
+}
+$(document).ready(function() {
+    $(".comment_form form").ajaxForm(comment_load);
 });
